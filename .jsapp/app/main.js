3f27ae3d483e1a7717b12997e6ea3ec150fd33f2
Mike Talley <miketalley@hotmail.com>
2014-11-29 16:41:29 -0500

    Changed global path

--- .jsapp/app/main.js
+++ .jsapp/app/main.js
@@ -6,23 +6,23 @@ requirejs.config({
     'transitions' : '../lib/durandal/js/transitions',
     'knockout': '../lib/knockout/knockout-3.2.0',
     'jquery': '../lib/jquery/jquery-2.1.1',
-    'globals': '/js/globals'
-    } 
+    'globals': 'js/globals'
+    }
 });
- 
+
 define(function (require) {
    var system = require('durandal/system'),
        app = require('durandal/app');
- 
+
    system.debug(true);
- 
+
    app.title = 'Create-a-Jam';
- 
+
    app.configurePlugins({
      router:true,
      dialog: true
    });
- 
+
    app.start().then(function() {
      app.setRoot('shell');
    });
