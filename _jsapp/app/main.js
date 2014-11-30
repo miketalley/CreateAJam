3f69132ceb94f3b1569f5e46f7cbf3cd55641880
Mike Talley <miketalley@hotmail.com>
2014-11-30 14:44:33 -0500

    Added json request acceptance for devise

--- _jsapp/app/main.js
+++ _jsapp/app/main.js
@@ -6,6 +6,7 @@ requirejs.config({
     'transitions' : '../lib/durandal/js/transitions',
     'knockout': '../lib/knockout/knockout-3.2.0',
     'jquery': '../lib/jquery/jquery-2.1.1',
+    'jquery.modal': '../lib/jquery-modal/jquery.modal.min',
     'globals': 'js/globals'
     }
 });
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   resources :homes
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   root to: "homes#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
