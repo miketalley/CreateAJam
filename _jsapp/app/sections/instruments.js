91e1920fb26bb9f076ba22c40057cb522011d6a9
Mike Talley <miketalley@hotmail.com>
2014-12-03 19:31:33 -0500

    Added instruments ajax request

--- _jsapp/app/sections/instruments.js
+++ _jsapp/app/sections/instruments.js
@@ -1,6 +1,21 @@
-define([], function () {
+define(['jquery', 'knockout'], function ($, ko) {
 
   function Instruments(){
+    var self = this;
+
+    self.allInstruments = ko.observable();
+
+    self.activate = function(){
+      $.get('http://jamsync.herokuapp.com/instruments.json')
+        .done(function(resp){
+          debugger;
+          if(resp && resp.success){
+            self.allInstruments(resp);
+          }
+        });
+    };
+
+
 
   }
 
